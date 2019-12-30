0b9659bd87f6c309895d59f8ebdcf290960b4afa
ohtaeg <otk1090@naver.com>
2019-12-30 14:32:05 +0900

    refactor (add method detach) : 준영속 관련 코드 추가

--- hello-jpa/src/main/java/com/ohtaeg/study/JpaMain.java
+++ hello-jpa/src/main/java/com/ohtaeg/study/JpaMain.java
@@ -7,7 +7,6 @@ import javax.persistence.Persistence;
 import java.util.List;
 
 public class JpaMain {
-
     final static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
 
     public static void main(String[] args) {
@@ -18,55 +17,12 @@ public class JpaMain {
         transaction.begin();
 
         try {
-            // 비영속
-            Member member = new Member();
-            member.setId(id);
-            member.setName("test");
+            executeJPQL(entityManager);
 
-            // 영속
-            entityManager.persist(member);
-
-            // select
-            Member findMember = entityManager.find(Member.class, id);
-            System.out.println("id = " + findMember.getId());
-            System.out.println("name = " + findMember.getName());
-
-            //executeJPQL(entityManager);
-
-            // 준영속
-            //entiryManager.detach(member);
+            detach(entityManager, id);
 
             // 삭제
             //entityManager.remove(findMember);
-
-            /**
-             * update
-             * jpa를 통해 엔티티를 갖고오게 되면 jpa가 관리를 해주기 때문에
-             * 트랜잭션 커밋시점에 데이터 변경시 update 쿼리를 만들어서 날린다.
-             * persist()를 사용안해도, 자바 컬렉션처럼 다룰 수 있음.
-             */
-            //findMember.setName("hello jpa");
-
-            /**
-             * JPQL
-             * 테이블이 아닌 객채 대상으로 탐색하기 때문에 일반 sql과는 형태가 다름.
-             * 방언에 맞게 쿼리가 변경된다.
-             * SELECT M
-             *   FROM Member M -> Merber 객체를 대상으로 한다.
-             *   alias를 통해 해당 객체의 필드들을 갖고올 수 있다.
-             *
-             *   쿼리 결과
-             *   select member0_.id as id1_0_,
-             *          member0_.name as name2_0_
-             *     from Member member0_
-             */
-            List<Member> members = entityManager.createQuery("SELECT M FROM Member M", Member.class)
-                                                .setFirstResult(1)
-                                                .setMaxResults(10) //paging - 1번부터 10개 갖고와라
-                                                .getResultList();
-
-            members.stream()
-                    .forEach(m -> System.out.println(m.getName()));
 
             transaction.commit();
         } catch (Exception e) {
@@ -79,6 +35,7 @@ public class JpaMain {
 
     // JPQL을 통한 플러시
     public static void executeJPQL(EntityManager entityManager) {
+        // 비영속
         Member member = new Member();
         member.setId(11L);
         member.setName("ohtaeg");
@@ -87,13 +44,50 @@ public class JpaMain {
         otherMember.setId(22L);
         otherMember.setName("ohtaeg2");
 
+        // 영속
         entityManager.persist(member);
         entityManager.persist(otherMember);
 
+        /**
+         * update (dirty checking)
+         * jpa를 통해 엔티티를 갖고오게 되면 jpa가 관리를 해주기 때문에
+         * 트랜잭션 커밋시점에 데이터 변경시 update 쿼리를 만들어서 날린다.
+         * persist()를 사용안해도, 자바 컬렉션처럼 다룰 수 있음.
+         */
+        otherMember.setName("hello jpa");
+
+        /**
+         * JPQL
+         * 테이블이 아닌 객채 대상으로 탐색하기 때문에 일반 SQL과 달리 형태가 다름.
+         * 방언에 맞게 쿼리가 변경된다.
+         * SELECT M
+         *   FROM Member M -> Merber 객체를 대상으로 한다.
+         *   alias를 통해 해당 객체의 필드들을 갖고올 수 있다.
+         *
+         *   쿼리 결과
+         *   select member0_.id as id1_0_,
+         *          member0_.name as name2_0_
+         *     from Member member0_
+         */
         List<Member> members = entityManager.createQuery("SELECT M FROM Member M", Member.class)
+                                            .setFirstResult(1)
+                                            .setMaxResults(10) //paging - 1번부터 10개 갖고와라
                                             .getResultList();
 
         members.stream()
                 .forEach(m -> System.out.println(m.getId() + ", " + m.getName()));
+    }
+
+    private static void detach(final EntityManager entityManager, final Long id) {
+        // select (영속)
+        Member findMember = entityManager.find(Member.class, id);
+        System.out.println("id = " + findMember.getId());
+        System.out.println("name = " + findMember.getName());
+
+        // 준영속
+        entityManager.detach(findMember);
+
+        // 준영속 - 영속성 컨텍스트 초기화
+        entityManager.clear();
     }
 }

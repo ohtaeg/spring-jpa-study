33c4944abb9b647823d58a1ca88d62d5d91c32da
ohtaeg <otk1090@naver.com>
2020-01-04 21:59:05 +0900

    refactor (add constraint) : 유니크 제약조건 추가 (이메일)

--- hello-jpa/src/main/java/com/ohtaeg/study/shop/domain/Member.java
+++ hello-jpa/src/main/java/com/ohtaeg/study/shop/domain/Member.java
@@ -5,13 +5,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR"
+        ,sequenceName = "MEMBER_SEQ" // 매핑할 DB 시퀀스 이름
+        ,initialValue = 1)
+@Table(uniqueConstraints = {
+        @UniqueConstraint(name ="unq_user_email", columnNames = {"email"})
+})
 public class Member {
-
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE
+            ,generator = "MEMBER_SEQ_GENERATOR")
     @Column(name = "MEMBER_ID")
     private Long id;
+
+    @Column(length = 10, nullable = false)
     private String name;
+
+    private String email;
     private String city;
     private String street;
     private String zipCode;
@@ -19,43 +30,49 @@ public class Member {
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
-    public Long getId() {
-        return id;
+    public Member() { }
+
+    public Member(final String name, final String email, final String city, final String street, final String zipCode) {
+        this.name = name;
+        this.email = email;
+        this.city = city;
+        this.street = street;
+        this.zipCode = zipCode;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
+    public void changeOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public void changeName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
+    public String getEmail() {
+        return email;
     }
 
     public String getCity() {
         return city;
     }
 
-    public void setCity(final String city) {
-        this.city = city;
-    }
-
     public String getStreet() {
         return street;
-    }
-
-    public void setStreet(final String street) {
-        this.street = street;
     }
 
     public String getZipCode() {
         return zipCode;
     }
 
-    public void setZipCode(final String zipCode) {
-        this.zipCode = zipCode;
+    public List<Order> getOrders() {
+        return orders;
     }
 }
